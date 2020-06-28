8b6bf04f546e2a82eceb2b81f67aaf9da2eceae3
Obinna Iheanachor <datascientistobi@gmail.com>
2020-06-28 21:56:43 +0100

    docs: Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bigausspy',
+      version='0.1',
+      description='Python package that calculates and plots Binomial and Gaussian probability distributions',
+      packages=['bigausspy'],
+      author= 'Obinna Iheanachor',
+      author_email= 'datascientistobi@mail.com',
+      zip_safe=False)
